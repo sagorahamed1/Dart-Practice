99fc5a69d29c1e3295ea4c57940d4cc4506f793c
sagorahamed1 <sagorahammed002@gmail.com>
2024-03-18 18:37:53 +0600

    for each

--- lib/list_practice.dart
+++ lib/list_practice.dart
@@ -1,11 +1,25 @@
-void main(){
-  List<int> listNo = [1,2,3,4,5,6,7,8,9,10];
-  List <String> namesList = ["Sagor Ahamed", "Palash", "Towhidul Islam", "Ahad Hossain", "Fayej", "Sahinur", "Aumi", "Afran kazi"];
+void main() {
+  List<int> listNo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  List<String> namesList = [
+    "Sagor Ahamed",
+    "Palash",
+    "Towhidul Islam",
+    "Ahad Hossain",
+    "Fayej",
+    "Sahinur",
+    "Aumi",
+    "Afran kazi"
+  ];
+  List<Friend> friendsList = [
+    Friend(name: "Sagor", age: 25),
+    Friend(name: "Siam", age: 30),
+    Friend(name: "Swapon", age: 23)
+  ];
+
 
 
   ///---------check list is empty or not----->
   print(listNo.isEmpty);
-
 
   ///---------name start with "A" letter------------>
   Iterable visiableName = namesList.where((element) {
@@ -13,9 +27,28 @@ void main(){
   });
   print('===>$visiableName');
 
-
   ///----------even or odd number print------->
   Iterable<int> evenNo = listNo.where((element) => (element % 2 == 0));
   print("====>$evenNo");
 
+  ///------------access all name by forEach ---------<>
+  namesList.forEach((element) {
+    print(element);
+  });
+
+  ///---------sum all number from a list-------------->
+  int sum = 0;
+  listNo.forEach((element) => sum += element);
+  print("===> $sum");
+
+
+  ///-------------access list with model class-------------------------->
+  friendsList.forEach((friend) => print("====> Name : ${friend.name} Age : ${friend.age}"));
+}
+
+class Friend {
+  final String name;
+  final int age;
+
+  Friend({required this.name, required this.age});
 }
