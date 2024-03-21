cbd21b7d88a24fbc88695679062158f3ea48a901
sagorahamed1 <sagorahammed002@gmail.com>
2024-03-21 17:39:48 +0600

    check map is empty or not

--- lib/map_practice.dart
+++ lib/map_practice.dart
@@ -3,43 +3,67 @@
 ///Value has to be Duplicate
 library;
 
-void main(){
-   Map <String, dynamic> names =
-   {
-     "name" : "Sagor Ahamed",
-     "age" : 25
-   };
+
+void main() {
+  Map<String, dynamic> names = {"name": "Sagor Ahamed", "age": 25};
 
 
-   ///-------------print this map-------------------->
-   print(names);
-
-   ///------------add key value in map--------------->
-   names["degenation"]= "Flutter Developer";
-   print("===> degenation add : ");
-
-   /// -----------access data by key----------------->
-   print("===> key :  ${names["name"]}");
-
-   ///-----------print map by for in loop----------->
-   for(var keys in names.keys){
-     print("===> keys :  ${keys}");
-   }
-
-
-   ///-----------print map by for in loop----------->
-   for(var value in names.values){
-     print("===> values : ${value}");
-   }
-
-   ///----------for each--------------------------->
-   names.forEach((key, value) => print("===> for Each : $key and $value"));
-
-   ///----------map update by update method-------->
-   names.update("age", (value) => 30);
-   print("===> update : $names");
+  ///-------------print this map-------------------->
+  print("===> names map : $names");
 
 
 
+  ///------------add key value in map--------------->
+  names["degenation"] = "Flutter Developer";
+  print("===> degenation add : ");
 
+
+
+  /// -----------access data by key----------------->
+  print("===> key :  ${names["name"]}");
+
+
+
+  ///-----------print map by for in loop----------->
+  for (var keys in names.keys) {
+    print("===> keys :  ${keys}");
+  }
+
+
+
+  ///-----------print map by for in loop----------->
+  for (var value in names.values) {
+    print("===> values : ${value}");
+  }
+
+
+
+  ///----------for each--------------------------->
+  names.forEach((key, value) => print("===> for Each : $key and $value"));
+
+
+
+  ///----------map update by update method-------->
+  names.update("age", (value) => 30);
+  print("===> update : $names");
+
+
+
+  ///---------remove from map by key name-------->
+  names.remove("name");
+  print("===> remove : ${names}");
+
+
+
+  ///--------check map is empty or not----------->
+  if (names.isEmpty) {
+    print("===> names isEmpty");
+  } else {
+    print("===> names isNotEmpty");
+  }
+
+
+  ///-------------map clear method---------------->
+  names.clear();
+  print("===> clear : $names");
 }
